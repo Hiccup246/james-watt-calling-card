5e20c56b202ab2c0a33a8c8f9b81e026f52db3f0
James Watt <james.watt529@outlook.com>
2022-09-24 18:46:51 +0100

    Create calling card modal, style modal

--- james-watt-calling-card.js
+++ james-watt-calling-card.js
@@ -7,6 +7,24 @@ class JamesWattCallingCard extends HTMLElement {
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
 
+    // Create modal
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "card-modal");
+
+    const modalContent = modal.appendChild(document.createElement("div"));
+    modalContent.setAttribute("class", "card-modal-content");
+
+    const modalTextOne = modalContent.appendChild(document.createElement("p"));
+    modalTextOne.innerText = "Curious one aren't you?";
+
+    const modalTextTwo = modalContent.appendChild(document.createElement("p"));
+    modalTextTwo.innerText =
+      "You have clicked on the calling card for the creator of this site...James. Watt";
+
+    const modalAnchor = modalContent.appendChild(document.createElement("a"));
+    modalAnchor.setAttribute("class", "modal-anchor");
+    modalAnchor.innerText = "Continue to his portfolio";
+
     // Apply external styles to the shadow DOM
     const googleFontApi = document.createElement("link");
     googleFontApi.setAttribute("rel", "preconnect");
@@ -74,9 +92,41 @@ class JamesWattCallingCard extends HTMLElement {
         text-decoration: underline;
         text-decoration-color: ${textColor};
       }
+
+      .card-modal {
+        width: 100%;
+        height: 100%;
+        background-color: white;
+        background-color: hsla(0,0%,4%,.86);
+        opacity: 0.7;
+        z-index: 999;
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: flex;
+      }
+
+      .card-modal-content {
+        background-color: white;
+        padding: 20px;
+        width: fit-content;
+        margin: auto auto;
+        text-align: center;
+        font-family: 'Space Mono', monospace;
+        font-size: 1em;
+        line-height: 1em;
+        border: 2px solid black;
+      }
+
+      .modal-anchor {
+        cursor: pointer;
+        text-decoration: underline;
+        text-decoration-color: black;
+      }
     `;
 
     this.shadowRoot.append(style, card);
+    this.shadowRoot.append(style, modal);
   }
 }
 
