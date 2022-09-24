18e22d4592c4b10688bebd7b784034979e3648d4
James Watt <james.watt529@outlook.com>
2022-09-24 23:07:25 +0100

    Refactor html element setup into methods

--- james-watt-calling-card.js
+++ james-watt-calling-card.js
@@ -3,29 +3,18 @@ class JamesWattCallingCard extends HTMLElement {
     super();
   }
 
+  disconnectedCallback() {}
+
   connectedCallback() {
-    // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
 
-    // Create modal
-    const modal = document.createElement("div");
-    modal.setAttribute("class", "card-modal");
+    this.setupGoogleFonts(shadow);
+    this.setupModal(shadow);
+    this.setupCard(shadow);
+    // this.setupOldModal(shadow);
+  }
 
-    const modalContent = modal.appendChild(document.createElement("div"));
-    modalContent.setAttribute("class", "card-modal-content");
-
-    const modalTextOne = modalContent.appendChild(document.createElement("p"));
-    modalTextOne.innerText = "Curious one aren't you?";
-
-    const modalTextTwo = modalContent.appendChild(document.createElement("p"));
-    modalTextTwo.innerText =
-      "You have clicked on the calling card for the creator of this site...James. Watt";
-
-    const modalAnchor = modalContent.appendChild(document.createElement("a"));
-    modalAnchor.setAttribute("class", "modal-anchor");
-    modalAnchor.innerText = "Continue to his portfolio";
-
-    // Apply external styles to the shadow DOM
+  setupGoogleFonts(shadow) {
     const googleFontApi = document.createElement("link");
     googleFontApi.setAttribute("rel", "preconnect");
     googleFontApi.setAttribute("href", "https://fonts.googleapis.com");
@@ -45,6 +34,79 @@ class JamesWattCallingCard extends HTMLElement {
     shadow.appendChild(googleFontApi);
     shadow.appendChild(googleFontGstatic);
     shadow.appendChild(googleFont);
+  }
+
+  setupModal(shadow) {
+    //bootstrap approach
+    const modalBackdrop = document.createElement("div");
+    modalBackdrop.setAttribute("class", "modal-backdrop");
+
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+
+    const modalDialog = modal.appendChild(document.createElement("div"));
+    modalDialog.setAttribute("class", "modal-dialog");
+
+    const modalText = modalDialog.appendChild(document.createElement("p"));
+    modalText.innerText = "Testing the internal text";
+
+    // Bootstrap work todo
+    // Add an event listener to our button to open the modal
+    // Opening the modal
+    // - Adds the modal backdrop from the drop (Opposite is removing it from the DOM)
+    // - Adds overflow: hidden to the document body (Opposite is that it is removed)
+    // - Adds display: block to the modal (opposite is display: none)
+    // Center align the modal
+
+    const style = document.createElement("style");
+    style.textContent = `
+      .modal-backdrop {
+        opacity: 0.5;
+        z-index: 1000;
+        background-color: black;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        transition: opacity, .15s, linear;
+      }
+
+      .modal {
+        z-index: 1001;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow-x: hidden;
+        overflow-y: auto;
+        outline: 0;
+        padding: 1rem;
+        margin: 0.5rem;
+      }
+
+      .modal-dialog {
+        position: relative;
+        width: auto;
+        margin: 0.5rem;
+        pointer-events: none;
+        background-color: white;
+        font-size: 1rem;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `;
+
+    shadow.append(style, modalBackdrop);
+    shadow.append(style, modal);
+  }
+
+  setupCard(shadow) {
+    // Work ToDo
+    // Update calling card width and height to be account for box-sizing: border-box
 
     // Create card element
     const card = document.createElement("div");
@@ -93,6 +155,31 @@ class JamesWattCallingCard extends HTMLElement {
         text-decoration-color: ${textColor};
       }
 
+      * {
+        box-sizing: border-box;
+      }
+    `;
+
+    shadow.append(style, card);
+  }
+
+  setupOldModal(shadow) {
+    // Create modal
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "card-modal");
+    const modalContent = modal.appendChild(document.createElement("div"));
+    modalContent.setAttribute("class", "card-modal-content");
+    const modalTextOne = modalContent.appendChild(document.createElement("p"));
+    modalTextOne.innerText = "Curious one aren't you?";
+    const modalTextTwo = modalContent.appendChild(document.createElement("p"));
+    modalTextTwo.innerText =
+      "You have clicked on the calling card for the creator of this site...James. Watt";
+    const modalAnchor = modalContent.appendChild(document.createElement("a"));
+    modalAnchor.setAttribute("class", "modal-anchor");
+    modalAnchor.innerText = "Continue to his portfolio";
+
+    const style = document.createElement("style");
+    style.textContent = `
       .card-modal {
         width: 100%;
         height: 100%;
@@ -123,10 +210,13 @@ class JamesWattCallingCard extends HTMLElement {
         text-decoration: underline;
         text-decoration-color: black;
       }
+
+      * {
+        box-sizing: border-box;
+      }
     `;
 
-    this.shadowRoot.append(style, card);
-    this.shadowRoot.append(style, modal);
+    shadow.appendChild(style, modal);
   }
 }
 
